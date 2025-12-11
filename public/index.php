24613bbcd65a80ab32f221278d54444b403d0d61
vittorgusttavo <victorgustavo08@hotmail.com>
2025-12-11 10:21:02 -0300

    Alterando index public

--- public/index.php
+++ public/index.php
@@ -6,16 +6,16 @@ use Illuminate\Contracts\Http\Kernel;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists(__DIR__ . '/../storage/framework/maintenance.php')) {
-    require __DIR__ . '/../storage/framework/maintenance.php';
+if (file_exists(__DIR__ . '/../laravel/storage/framework/maintenance.php')) {
+    require __DIR__ . '/../laravel/storage/framework/maintenance.php';
 }
 
 // Register the Composer autoloader...
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../laravel/vendor/autoload.php';
 
 
 // Run the application
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../laravel/bootstrap/app.php';
 $app->alias('request', Request::class);
 
 $kernel = $app->make(Kernel::class);
